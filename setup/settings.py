a5eacb4641040f330dc8978106dc680ec0bf37e0
Bárbara <6abi.cardoso@gmail.com>
2021-07-06 21:10:34 -0300

    Incluindo filtro para ordenação dos JSONs

--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'clientes'
+    'clientes',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
